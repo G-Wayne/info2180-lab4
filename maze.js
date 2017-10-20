b704fc3b385821045e1041d1c4aae249a5f73262
G-Wayne <32604878+G-Wayne@users.noreply.github.com>
2017-10-20 19:41:32 -0400

    This is Exercise 1

---
+++ maze.js
@@ -0,0 +1,7 @@
+window.onload = function() {
+  document.getElementById('boundary1').onmouseover = start;
+}
+
+function start(){
+  document.getElementById('boundary1').className = "boundary youlose";
+}

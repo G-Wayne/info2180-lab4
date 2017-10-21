202e34c849749f3e7b813ed7cc53931efe116b2c
G-Wayne <32604878+G-Wayne@users.noreply.github.com>
2017-10-20 22:26:38 -0400

    This is Exercise 2

--- maze.js
+++ maze.js
@@ -1,7 +1,20 @@
 window.onload = function() {
-  document.getElementById('boundary1').onmouseover = start;
+
+   document.getElementById('boundary1').onmouseover=start;
+   document.getElementById('boundary1').addEventListener("mouseover",borders);
 }
 
-function start(){
-  document.getElementById('boundary1').className = "boundary youlose";
+function start() {
+
+	document.getElementById('boundary1').className="boundary youlose";
+}
+
+function borders(){
+
+	var walls = document.querySelectorAll(".boundary");
+	for (var x=0;x < walls.length; x++)
+	{
+		walls[x].className = "boundary youlose";
+		document.getElementById("status").textContent = "You Lose!";
+	}
 }

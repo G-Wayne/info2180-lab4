4cfcd83f00bac050041caf1b79ce42fc4af45517
G-Wayne <32604878+G-Wayne@users.noreply.github.com>
2017-10-21 09:36:42 -0400

    This is Exercise 6
    Exercise 5 already completed

--- maze.js
+++ maze.js
@@ -1,11 +1,12 @@
 var line = document.querySelectorAll(".boundary");
- var touch = "false";
- window.onload = function(){
+var touch = "false";
+window.onload = function(){
     var move= document.getElementById("boundary1");
     borders();
     startOver();
     end(touch);
-
+    setupMaze();
+    cheatBlocker();
  }
 
  function start(cursor){
@@ -14,30 +15,30 @@ var line = document.querySelectorAll(".boundary");
          touch="true";
          var line = document.querySelectorAll(".boundary");
          for(i = 0; i < line.length-1; i++){
-        line[i].className="boundary youlose";
-
+         line[i].className="boundary youlose";
      }
-
      }
-
  };
+
  function borders(){
       var line = document.querySelectorAll(".boundary");
      for(i = 0; i < line.length-1; i++){
         start(line[i])
      }
  };
+
  function end(error){
      document.getElementById("end").addEventListener("mouseover",outcome);
      function outcome(){
      if(document.getElementById("boundary1").className==="boundary youlose"){
-         document.getElementById("status").textContent="You Lose!"
+        document.getElementById("status").textContent="You Lose!"
      }
      else{
          document.getElementById("status").textContent = "You Win! :) :)"
      }
  };
  };
+
 function startOver(){
     document.getElementById("start").addEventListener("click",reset);
     function reset(){
@@ -48,5 +49,33 @@ function startOver(){
          }
          document.getElementById("status").textContent = 'Use your mouse to press the "S" to restart.'
           }
+}
 
+function setupMaze(){
+    document.getElementById("start").addEventListener("mouseout",startMaze);
+    function startMaze(){
+        document.getElementById("status").textContent = 'Begin your journey'
+    }
+
+}
+function cheatBlocker(){
+     document.getElementById("maze").addEventListener("mouseout",blockCheat);
+     function blockCheat(event){
+        var x = event.clientX;
+        var y = event.clientY;
+
+        var top = document.getElementById("maze").offsetTop;
+        var height = document.getElementById("maze").offsetHeight;
+        var left = document.getElementById("maze").offsetLeft;
+        var width = document.getElementById("maze").offsetWidth;
+
+         if(touch && x < left || y < top || y > (top + height) ){
+             document.getElementById("status").textContent = "You Cheated You Lose!";
+
+             var line = document.querySelectorAll(".boundary");
+         for(i = 0; i < line.length-1; i++){
+         line[i].className="boundary youlose";
+         }
+     }
+}
 }

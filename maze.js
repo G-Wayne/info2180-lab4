a7a07e38f16aa9f7a87a39dfd1b1a89337eb7c18
G-Wayne <32604878+G-Wayne@users.noreply.github.com>
2017-10-21 00:25:20 -0400

    This is Exercise 3

--- maze.js
+++ maze.js
@@ -1,20 +1,45 @@
-window.onload = function() {
+var touch = "false";
+ window.onload = function(){
+    var move= document.getElementById("boundary1");
+   start(move);
+   borders();
+   end(touch);
+ }
 
-   document.getElementById('boundary1').onmouseover=start;
-   document.getElementById('boundary1').addEventListener("mouseover",borders);
+ function start(cursor){
+     cursor.addEventListener("mouseover",glow);
+
+     function glow(){
+        touch="true";
+         var line = document.querySelectorAll(".boundary");
+        for(i in line){
+        for(i = 0; i < line.length-1; i++){
+        line[i].className="boundary youlose";
+     }
+     }
+
 }
-
-function start() {
-
-	document.getElementById('boundary1').className="boundary youlose";
+};
+ function borders(){
+      var line = document.querySelectorAll(".boundary");
+    for(i in line){
+    for(i = 0; i < line.length-1; i++){
+        start(line[i])
+     }
 }
+};
 
-function borders(){
 
-	var walls = document.querySelectorAll(".boundary");
-	for (var x=0;x < walls.length; x++)
-	{
-		walls[x].className = "boundary youlose";
-		document.getElementById("status").textContent = "You Lose!";
-	}
-}
+function end(error){
+    document.getElementById("end").addEventListener("mouseover",outcome);
+    function outcome(){
+
+    if(document.getElementById("boundary1").className==="boundary youlose"){
+        document.getElementById("status").textContent="You Lose!"
+    }
+    else{
+        document.getElementById("status").textContent = "You Win! :) :)"
+
+    }
+};
+};

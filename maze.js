26aa1214f94997f21c4f6374837d976144c292c1
G-Wayne <32604878+G-Wayne@users.noreply.github.com>
2017-10-21 02:01:28 -0400

    This is Exercise 4

--- maze.js
+++ maze.js
@@ -1,45 +1,52 @@
-var touch = "false";
+var line = document.querySelectorAll(".boundary");
+ var touch = "false";
  window.onload = function(){
     var move= document.getElementById("boundary1");
-   start(move);
-   borders();
-   end(touch);
+    borders();
+    startOver();
+    end(touch);
+
  }
 
  function start(cursor){
      cursor.addEventListener("mouseover",glow);
-
      function glow(){
-        touch="true";
+         touch="true";
          var line = document.querySelectorAll(".boundary");
-        for(i in line){
-        for(i = 0; i < line.length-1; i++){
+         for(i = 0; i < line.length-1; i++){
         line[i].className="boundary youlose";
-     }
+
      }
 
-}
-};
+     }
+
+ };
  function borders(){
       var line = document.querySelectorAll(".boundary");
-    for(i in line){
-    for(i = 0; i < line.length-1; i++){
+     for(i = 0; i < line.length-1; i++){
         start(line[i])
      }
+ };
+ function end(error){
+     document.getElementById("end").addEventListener("mouseover",outcome);
+     function outcome(){
+     if(document.getElementById("boundary1").className==="boundary youlose"){
+         document.getElementById("status").textContent="You Lose!"
+     }
+     else{
+         document.getElementById("status").textContent = "You Win! :) :)"
+     }
+ };
+ };
+function startOver(){
+    document.getElementById("start").addEventListener("click",reset);
+    function reset(){
+         var line = document.querySelectorAll(".boundary");
+         for(i = 0; i < line.length-1; i++){
+            line[i].className="boundary";
+
+         }
+         document.getElementById("status").textContent = 'Use your mouse to press the "S" to restart.'
+          }
+
 }
-};
-
-
-function end(error){
-    document.getElementById("end").addEventListener("mouseover",outcome);
-    function outcome(){
-
-    if(document.getElementById("boundary1").className==="boundary youlose"){
-        document.getElementById("status").textContent="You Lose!"
-    }
-    else{
-        document.getElementById("status").textContent = "You Win! :) :)"
-
-    }
-};
-};
